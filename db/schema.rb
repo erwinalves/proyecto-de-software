47d729a9e295781d036c8b92ed4f477af46ea7ac
victorgomeze <gs_kototo@hotmail.com>
2017-02-06 13:40:29 -0300

    actualizacion devise y tablas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117164927) do
+ActiveRecord::Schema.define(version: 20170204172538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20170117164927) do
   create_table "documentos", force: :cascade do |t|
     t.integer  "tipo_documento"
     t.integer  "cantidad_producto"
-    t.integer  "rut_proveedor"
+    t.string   "rut_proveedor"
     t.string   "fecha_emision"
     t.float    "valor_neto"
     t.float    "valor_exento"
@@ -66,16 +66,6 @@ ActiveRecord::Schema.define(version: 20170117164927) do
     t.datetime "updated_at",        null: false
   end
 
-  create_table "personas", force: :cascade do |t|
-    t.string   "nombre"
-    t.integer  "rol"
-    t.integer  "rut"
-    t.integer  "telefono"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "users_id"
-  end
-
   create_table "productos", force: :cascade do |t|
     t.integer  "stock_actual"
     t.integer  "stock_minimo"
@@ -84,24 +74,6 @@ ActiveRecord::Schema.define(version: 20170117164927) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
-
-  create_table "pruebas", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
-  add_index "pruebas", ["email"], name: "index_pruebas_on_email", unique: true, using: :btree
-  add_index "pruebas", ["reset_password_token"], name: "index_pruebas_on_reset_password_token", unique: true, using: :btree
 
   create_table "registros", force: :cascade do |t|
     t.datetime "ingreso"
@@ -123,30 +95,17 @@ ActiveRecord::Schema.define(version: 20170117164927) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.string   "name"
+    t.string   "permission_level"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "rol"
+    t.integer  "rut"
+    t.integer  "telefono"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "usuarios", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
-  add_index "usuarios", ["email"], name: "index_usuarios_on_email", unique: true, using: :btree
-  add_index "usuarios", ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "carros", "productos", on_delete: :cascade
 end

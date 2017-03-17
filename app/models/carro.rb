91d804d9d5c434d4523e5ffd8902f6533c7c64fb
victorgomeze <gs_kototo@hotmail.com>
2017-03-17 13:09:05 -0300

    Ultimos ajustes

--- app/models/carro.rb
+++ app/models/carro.rb
@@ -2,4 +2,10 @@ class Carro < ActiveRecord::Base
   belongs_to :product
   belongs_to :ticket
   belongs_to :invoices
+  validates :ticket_id, format: { with: /\A[0-9]+\z/, message: "Solo se aceptan numeros" }
+  validates :product_id, format: { with: /\A[0-9]+\z/, message: "Solo se aceptan numeros" }
+  validates :cantidad_producto, format: { with: /\A[0-9]+\z/, message: "Solo se aceptan numeros" }
+    validates :ticket_id, presence: {message: "No debe estar vacio"}, confirmation: true
+	validates :product_id, presence: {message: "No debe estar vacio"}, confirmation: true
+	validates :cantidad_producto, presence: {message: "No debe estar vacio"}, confirmation: true
 end
